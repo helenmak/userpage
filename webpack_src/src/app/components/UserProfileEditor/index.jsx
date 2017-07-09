03de1c6165732612a03f8e343226a02e6f68ac02
helenmak <elenamak@ukr.net>
2017-07-10 01:49:46 +0300

    email field disabled

--- webpack_src/src/app/components/UserProfileEditor/index.jsx
+++ webpack_src/src/app/components/UserProfileEditor/index.jsx
@@ -1,53 +1,56 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-
-import MainPage from 'app/containers/MainPage';
-import * as authSuccess from 'app/actions/auth';
+import * as authActions from 'app/actions/auth'
 
 import style from './style.scss';
 
 class UserEditorForm extends React.Component {
 	constructor(props){
 		super(props);
-		this.state = {
-			// 'Name': '',
-			// 'E-mail': '',
-			// 'Phone': ''
-		};
-	 	this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleChange = this.handleChange.bind(this);
+		this.state = {
+		}
 	}
-
-	//To Do: если неподтверждённый емейл, то кнопка "Отправить заново"
-	//добавить пол, возраст, аллергии
 
 	renderFields = () => {
 		if (this.props.userData && this.props.userId) {
-			let field_name = ['Name', 'username', 'text', `${this.props.userData[this.props.userId].username}`];
-			let field_birthday = ['Birthday', 'birthday', 'date', '']; //убрать
-			let field_phone = ['Phone', 'phone', 'tel', ''];
-			// let bt_plusPhone = ['Add number', 'user_plusPhone', 'button'];
-			let fields = [field_name, field_birthday, field_phone];
+			let user = this.props.userData[this.props.userId];
+			const {username, email, birthday, gender, phones} = user;
 
-			let arrFields = [];
-			for (let i = 0; i < fields.length; i++) {
-				arrFields.push(
-					<label key={fields[i][1]} htmlFor={fields[i][1]}>
-					<h4>{fields[i][0]}</h4>
-					<input type={fields[i][2]} id={fields[i][1]}
-						   //defaultValue={fields[i][3]}
-						   value={this.state[fields[i][0]]}
-						   onChange={this.handleChange} />
+			let field_name = ['Name', 'username', 'text', username];
+			let field_birthday = ['Birthday', 'birthday', 'date', birthday]; //маска ввода, дата пикер
+			let field_phones = ['Phone', 'phones', 'tel', phones[0]];
+			let field_email = ['Email', 'email', 'email', email];
+			let field_gender = ['Gender', 'gender', 'text', gender];
+
+			let all_fields = [field_name, field_phones, field_gender, field_birthday, field_email];
+			let formFields = [];
+
+			for (let i = 0; i < all_fields.length; i++) {
+				let inaccessible_input = <input type={all_fields[i][2]} id={all_fields[i][1]}
+												defaultValue={all_fields[i][3]}
+												readOnly />;
+				let accessible_input = <input type={all_fields[i][2]} id={all_fields[i][1]}
+												defaultValue={all_fields[i][3]}
+												value={this.state[all_fields[i][1]]}
+												onChange= {this.handleChange} />;
+				formFields.push(
+					<label key={all_fields[i][1]} htmlFor={all_fields[i][1]}>
+						<h4>{all_fields[i][0]}</h4>
+						{all_fields[i][2] === 'email'? inaccessible_input : accessible_input}
 					</label>
-				)}
+				)
+			}
 
 			return (
 				<div>
 					<h2>Edit Profile</h2>
-					{arrFields}
+					{formFields}
 					<input type="submit" value="Save"/>
-				</div> )
+				</div>
+			)
 		} else {
 			return (
 				<div>
@@ -58,17 +61,21 @@ class UserEditorForm extends React.Component {
 
 	handleChange(event){
 		let name = event.target.id;
-	 	this.setState({[name]: event.target.value, id: this.props.userId});
+	 	this.setState({[name]: event.target.value, id: this.props.userId, modifyTime: new Date});
 	}
 
-	handleSubmit(event) {
+	handleSubmit(event){
 		event.preventDefault();
-		console.log(this.state);
+		//console.log(this.state);
+		const { changeUserData } = this.props.actions;
+		changeUserData(this.state);
 		let options = { method: 'POST',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify(this.state) };
 		fetch('/save_user_data', options)
-			 //.then((response)=>response.json)
+			 .then((response)=> console.log(response))
+				 //response.json)
+
 	}
 
 	render() {
@@ -76,9 +83,9 @@ class UserEditorForm extends React.Component {
 			<form name ='profileEditor' className={style.form} onSubmit={this.handleSubmit}>
 				{this.renderFields()}
 			</form>
-
 		)
 	}
+
 }
 
 const mapStateToProps = (state)=>{ //from reducers
@@ -88,5 +95,8 @@ const mapStateToProps = (state)=>{ //from reducers
 	}
 };
 
+const mapDispatchtoProps = (dispatch)=>{//change user data in store
+	return { actions: bindActionCreators(authActions, dispatch) }
+};
 
-export default connect(mapStateToProps)(UserEditorForm);
+export default connect(mapStateToProps, mapDispatchtoProps)(UserEditorForm);
