95d692522cb52dd7bbfad3a6847d091ab5929acb
helenmak <elenamak@ukr.net>
2017-07-10 14:01:14 +0300

    "change_userdata", copying action in reducer

--- webpack_src/src/app/reducers/users/index.js
+++ webpack_src/src/app/reducers/users/index.js
@@ -4,23 +4,31 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 
-	let _state = { ...state }
+	let _state = { ...state }, _action = {...action};
 
-	switch (action.type) {
+	switch (_action.type) {
+		case 'CHANGE_USERDATA':
+			for (let key in _action.payload) {
+				_state[_action.payload.id][key] = _action.payload[key]
+			}
+			break;
 		case 'AUTH_SUCCESS':
 			_state = {
 				...state,
-				[action.payload._id]: {
-					username: action.payload.username,
-					email: action.payload.email,
-					uid: action.payload._id
+				[_action.payload._id]: {
+					username: _action.payload.username,
+					email: _action.payload.email,
+					uid: _action.payload._id,
+					birthday: _action.payload.birthday,
+					gender: _action.payload.gender,
+					phones: _action.payload.phones,
+					createTime: _action.payload.createTime
 				}
 			}
 			break;
 		default:
 			return state;
 	}
-
 	return _state;
 }
 
