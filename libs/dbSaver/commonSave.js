62cf59ce9e2e3c8bfcd49ffe44a35327c573b517
helenmak <elenamak@ukr.net>
2017-07-11 19:09:51 +0300

    datasaving 'app' listeners added

--- libs/dbSaver/commonSave.js
+++ libs/dbSaver/commonSave.js
@@ -1,6 +1,6 @@
 const usersDB = require('../user');
 const dbSaver = require('./dbSaver');
-
+const app = require('http_server');
 
 const saveModelsToDb = function(...models){
     models.forEach(function (docs){
@@ -9,7 +9,14 @@ const saveModelsToDb = function(...models){
                 continue;
             }
             console.log(`${docs[document]} ${docs[document].isModified()}`)
-            docs[document].save();
+            docs[document].save()
+                .then((result)=>{
+                    app.emit('userdata_saved', result);
+                })
+                .catch((err)=>{
+                    app.emit('userdata_not_saved', err);
+                    //throw err;
+                })
         }
     })
 
--- routes/saveUserData.js
+++ routes/saveUserData.js
@@ -1,16 +1,24 @@
 const users = require('libs/user');
 const express = require('express');
 const router = express.Router();
+const app = require('http_server');
 
 router.post('/', function (req, res) {
     console.log(req.body);
     for( let key in req.body) {
         if(key !== 'id' && users[req.body.id]) {
             users[req.body.id][key] = req.body[key];
-            users[req.body.id].modifyTime = req.body.modifyTime;
         }
     }
-    res.send('Data will be saved')
+
+    res.send()
+});
+
+app.on('userdata_not_saved', (err)=>{
+    console.log('New user data were not saved because of error: '+err);
+});
+app.on('userdata_saved', (data)=>{
+    console.log('Data were saved');
 });
 
 module.exports = router;
