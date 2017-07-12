6e4a47d3018393550d421839323baacffd60b92a
helenmak <elenamak@ukr.net>
2017-07-13 01:09:16 +0300

    back-end Date check

--- routes/saveUserData.js
+++ routes/saveUserData.js
@@ -6,12 +6,20 @@ const app = require('http_server');
 router.post('/', function (req, res) {
     console.log(req.body);
     for( let key in req.body) {
+
         if(key !== 'uid' && users[req.body.uid]) {
+            if(users[req.body.uid][key] instanceof Date){
+                //откинуть числа и значения_не_даты
+                //знач. напр.'2007-' конвертируется нормально, вопреки логике. Что делать?
+                if (new Date(req.body[key]) == 'Invalid Date' || parseInt(req.body[key]) == +req.body[key]) {
+                    req.body[key] = users[req.body.uid][key]
+                }
+            }
             users[req.body.uid][key] = req.body[key];
         }
     }
 
-    res.send('data saved')
+    res.send('Saved')
 });
 
 app.on('userdata_not_saved', (err)=>{
